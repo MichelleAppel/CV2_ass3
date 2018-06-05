c16abb4cafb4597797695891940d5a40daa14b17
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-06-05 13:25:24 +0200

    test

--- Code/main.cpp
+++ Code/main.cpp
@@ -80,7 +80,7 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformPointCloud(pcl::PointCloud<pcl::
 ///////// added /////////
 pcl::PointCloud<pcl::PointXYZ>::Ptr depthToPointCloud(cv::Mat depth_image, double focal_length) {
 	cout << "depthToPointCloud" << "\n";
-	cout << focal_length << "\n"
+	cout << focal_length << "\n";
 	
 	// define new PointXYZ
 	pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud(new pcl::PointCloud<pcl::PointXYZ>());
@@ -92,7 +92,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr depthToPointCloud(cv::Mat depth_image, doubl
 	    	point.x = j / focal_length; //depth_image.at<float>(i, j);
 	    	point.y = i / focal_length; //depth_image.at<float>(i, j)
 	    	point.z = depth_image.at<float>(i, j) / focal_length;
-			cout << point.z << "\n"
+			cout << point.z << "\n";
 			
   
 	    	point_cloud->points.push_back(point);
