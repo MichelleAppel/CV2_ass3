2cb85746bf0f0f936b76f05335ed286d6891a989
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-06-05 17:03:40 +0200

    test

--- Code/main.cpp
+++ Code/main.cpp
@@ -172,7 +172,7 @@ pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr mergingPointClouds(Frame3D frames[]
 		// 1.  point cloud <- depthToPointCloud(depth image, focal length)
 		pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloud(new pcl::PointCloud<pcl::PointXYZ>());
 		// pointCloud = depthToPointCloud(depthImage, focalLength);
-		pointCloud = mat2IntegralPointCloud(depthImage, focalLength, 2.5);
+		pointCloud = mat2IntegralPointCloud(depthImage, focalLength, 0.5);
 		
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloudRGB(new pcl::PointCloud<pcl::PointXYZRGB>());
 		//pointCloudRGB = depthToPointCloudRGB(depthImage, focalLength);
